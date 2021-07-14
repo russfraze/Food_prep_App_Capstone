b43881ab366e79a17ccba29b153239b3e33e28ef
Russell Fraze <russell.fraze@gmail.com>
2021-07-14 16:48:06 -0700

    save settings

--- Big_Shop_project/Big_Shop_project/settings.py
+++ Big_Shop_project/Big_Shop_project/settings.py
@@ -1,6 +1,10 @@
 import django_on_heroku
-from . import secrets
+# from . import secrets
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--@p5=5sugiz=(3t8l^(-=nq3(3=8g2sm95yeh@p2ema3fsw4@3'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
